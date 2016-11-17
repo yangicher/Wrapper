4941036fdb85ece4cc5dacc06038fc881579cdf8
sergey.yangicher <sergey.yangicher@plarium.com>
2016-11-17 07:21:04 +0200

    pr bar

--- Assets/Wrapper/Scripts/Test.cs
+++ Assets/Wrapper/Scripts/Test.cs
@@ -10,16 +10,39 @@ namespace Assets.Wrapper.Scripts
         [SerializeField]
         private Button _startButton;
 
+		[SerializeField]
+		private Image _gImg;
+
+		[SerializeField]
+		private Image _bImg;
+
+		[SerializeField]
+		private GameObject _pointer;
+
+
         // Update is called once per frame
         void Update()
         {
             if (Input.GetKeyDown(KeyCode.S))
             {
-
+				_gImg.fillAmount = 0.4f;
+				_bImg.fillAmount = 0.8f;
             }
-            else if (Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKeyDown(KeyCode.Space))
             {
-                
+				var xPos = 0f;
+				if (_gImg.fillAmount > 0.5f) {
+					xPos = _gImg.rectTransform.position.x + (_gImg.rectTransform.rect.width * (_gImg.fillAmount - 0.5f));
+				} else if (_gImg.fillAmount < 0.5f) {
+					xPos = _gImg.rectTransform.position.x - (_gImg.rectTransform.rect.width * (0.5f - _gImg.fillAmount));
+				} else {
+					xPos = _gImg.rectTransform.position.x;
+				}
+
+				_pointer.transform.parent = _gImg.transform;
+				_pointer.transform.position = Vector3.zero;
+				_pointer.transform.position = new Vector3(xPos, _gImg.transform.position.y, _gImg.transform.position.z);
+				_pointer.transform.rotation = _gImg.transform.rotation;
             }
         }
 
